76433668bbb0a2d069ad9536a0b586b8cdb39697
NicolasKonishi <onishinicolas8@gmail.com>
2024-03-30 17:50:13 -0300

    alteração

---
+++ Santander-dev-week-2024/src/main/java/me/dio/sdw24/application/AskChampionsUseCase.java
@@ -0,0 +1,21 @@
+package me.dio.sdw24.application;
+
+import me.dio.sdw24.domain.exception.ChampionNotFoundException;
+import me.dio.sdw24.domain.model.Champion;
+import me.dio.sdw24.domain.ports.ChampionsRepository;
+
+import java.util.List;
+
+public record AskChampionsUseCase(ChampionsRepository repository) {
+
+    public String askChampion(Long championId, String question){
+
+        Champion champion = repository.findById(championId)
+                .orElseThrow(() -> new ChampionNotFoundException(championId));
+
+        String championContext = champion.generateContexByQuestion(question);
+
+        //todo: Evoiluir a lógica para considerar a integração com IA para o retorno
+        return championContext;
+    }
+}
